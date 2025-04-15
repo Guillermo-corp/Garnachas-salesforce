924bf3fdc32e767d494a3c701f07d129c32fad80
Guillermo Mendez <202160504@ucc.mx>
2025-04-15 15:09:42 -0600

    .

--- garnachas-salesforce/src/app/app.config.ts
+++ garnachas-salesforce/src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http'; // Importa el proveedor de HttpClient
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient() // Agrega el proveedor de HttpClient aquí
+  ]
 };
