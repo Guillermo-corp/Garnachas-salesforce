41d76185e5189a08892c69da29177eb93aec0e4d
Guillermo Mendez <202160504@ucc.mx>
2025-04-15 16:38:58 -0600

    cambio de logica

--- garnachas-salesforce/dist/garnachas-salesforce/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-C3SZETR6.mjs";import"./chunk-33BUSKAB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- garnachas-salesforce/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    stripePublicKey: 'pk_test_51RDonfDuneb1ckN1mjIju8Y9HTTYGTcuirT9Eqbn2cPeFuvrGH0baG9cbhKGm0Y2XRX9tOdbHymoHZe1QttgeNra00pz8lizQJ',
-  };
