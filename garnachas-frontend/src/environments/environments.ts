0006d12a66e58904638d9357f668b09a7c00a236
Guillermo Mendez <202160504@ucc.mx>
2025-04-29 18:12:03 -0600

    modificacion de llaves

--- garnachas-frontend/src/environments/environments.ts
+++ garnachas-frontend/src/environments/environments.ts
@@ -10,7 +10,7 @@
     production: false,
     salesforce: {
       baseUrl: 'https://orgfarm-7bd48eb5b5-dev-ed.develop.my.salesforce.com/services/data/v62.0/sobjects/Purchase__c', // Reemplaza con tu instancia y versión de API
-      accessToken: '00DgK000001Krim!AQEAQNl4LCQDNPg.bbe22ggoxehN90RP8tPzieYvSCy.xioxBK_7AQAUk.4Pm7Vx1Sbli5HcDjKpKhLqFh1x0JJ101H1cZv_', // Reemplaza con tu token de acceso
+      accessToken: '00DgK000001Krim!AQEAQLOeqfwKvWDjN.GrNTbcFEEZHPW2GaZxgKwXyuz6XU6AqnqRn7FWfMFzGaoBCq0vxGO5OuHTFkAPxa.9.MKPR7Spa.hs', // Reemplaza con tu token de acceso
       refreshToken: '5Aep8612EC5NxGKVYreGs13NIzFX7k8UaPFqYWXrlgaxCIYFsUGPSoD_VC2ih2mlKXmnl.pCzuOSmgHKUgK2D0F', // Agrega el Refresh Token
       clientId: '3MVG9rZjd7MXFdLgVUB54wy8BfunvX_6eaKWUMMprEvNxTad6sEc1gUpXtzUAbf_bxaWWutBwd_RVb1VW_TJR', // Reemplaza con tu Consumer Key
       clientSecret: '16A9B63899FA1F1133E8608900F75C1072BAAF411A9CB7DDF968092CD4A6BCE8', // Reemplaza con tu Consumer Secret
