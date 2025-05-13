12389fec538534c64663e2ba6916fbe3e9af894c
Guillermo Mendez <202160504@ucc.mx>
2025-05-13 17:23:52 -0600

    actualizacion de token

--- garnachas-frontend/src/environments/environments.ts
+++ garnachas-frontend/src/environments/environments.ts
@@ -11,8 +11,8 @@
     salesforce: {
       baseUrl: 'https://orgfarm-7bd48eb5b5-dev-ed.develop.my.salesforce.com', // Solo la instancia base
 /*       baseUrl: 'https://orgfarm-7bd48eb5b5-dev-ed.develop.my.salesforce.com/services/data/v62.0/sobjects/Purchase__c', // Reemplaza con tu instancia y versión de API
- */   accessToken: '00DgK000001Krim!AQEAQMXFdo6S8Ye7CqRh9S6NcDenJolmTvmXHy6qkaIM0yK0YXSFfUVW2IfTb2VeltFhUemj3JhVFL.Pot.lUYI_ykgc.QC6', // Reemplaza con tu token de acceso
-      refreshToken: '5Aep8612EC5NxGKVYreGs13NIzFX7k8UaPFqYWXrlgaxCIYFsVrodN5eMnanxemVF2PtYvIzAxzeDaOHiZ1TKxp', // Agrega el Refresh Token
+ */   accessToken: '00DgK000001Krim!AQEAQE5m_HFTdz.Li0LYRavu614sbI4cvcHhf70LR62fzDwYWX.d95ZwXNEoLIqPUXobbJW323H5oqOR1LN58WQ6ENw83ajU', // Reemplaza con tu token de acceso
+      refreshToken: '5Aep8612EC5NxGKVYreGs13NIzFX7k8UaPFqYWXrlgaxCIYFsUid1St_YKp0FebTl7M1DItuphkMnq_XTxrrMIn', // Agrega el Refresh Token
       clientId: '3MVG9rZjd7MXFdLgVUB54wy8BfunvX_6eaKWUMMprEvNxTad6sEc1gUpXtzUAbf_bxaWWutBwd_RVb1VW_TJR', // Reemplaza con tu Consumer Key
       clientSecret: '16A9B63899FA1F1133E8608900F75C1072BAAF411A9CB7DDF968092CD4A6BCE8', // Reemplaza con tu Consumer Secret
       tokenUrl: 'https://login.salesforce.com/services/oauth2/token', // Endpoint para renovar el token
