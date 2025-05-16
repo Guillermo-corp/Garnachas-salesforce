1bc11691e244f5af082834ea5fe32c9d4e74878a
Guillermo Mendez <202160504@ucc.mx>
2025-05-16 09:31:56 -0600

    cambio de token

--- garnachas-frontend/src/environments/environment.prod.ts
+++ garnachas-frontend/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: false,
+  production: true,
   salesforce: {
-    baseUrl: 'https://orgfarm-7bd48eb5b5-dev-ed.develop.my.salesforce.com/services/data/v62.0/sobjects/Purchase__c', // Reemplaza con tu instancia y versión de API
-    accessToken: '00DgK000001Krim!AQEAQK.kEPj6TezgC0FGHwy0P0zWd_qqUuTTHFJA_USMdOrBaKpLjyjtg023Bl3VNMKGtaARKSFkUAx0PkRMzv_SC.RF7rl.', // Reemplaza con tu token de acceso
-    refreshToken: '5Aep8612EC5NxGKVYreGs13NIzFX7k8UaPFqYWXrlgaxCIYFsVU1tpwWm97LgmUnJ0MtkKvt3pYHyN2rKowau1Z', // Agrega el Refresh Token
+    baseUrl: 'https://orgfarm-7bd48eb5b5-dev-ed.develop.my.salesforce.com', // Solo la instancia base
+    accessToken: '00DgK000001Krim!AQEAQPT9uc7YT4ve1DrtNEmKrvFPXBtEII.svpGu7rXOehm_YGYnsQJtAQS0Yr9D7wSm5nxg0YKKjk231MLccq4cMedorFYh', // Reemplaza con tu token de acceso
+    refreshToken: '5Aep8612EC5NxGKVYreGs13NIzFX7k8UaPFqYWXrlgaxCIYFsUid1St_YKp0FebTl7M1DItuphkMnq_XTxrrMIn', // Agrega el Refresh Token
     clientId: '3MVG9rZjd7MXFdLgVUB54wy8BfunvX_6eaKWUMMprEvNxTad6sEc1gUpXtzUAbf_bxaWWutBwd_RVb1VW_TJR', // Reemplaza con tu Consumer Key
     clientSecret: '16A9B63899FA1F1133E8608900F75C1072BAAF411A9CB7DDF968092CD4A6BCE8', // Reemplaza con tu Consumer Secret
     tokenUrl: 'https://login.salesforce.com/services/oauth2/token', // Endpoint para renovar el token
   },
-};
+}; 
--- garnachas-frontend/src/environments/environments.ts
+++ garnachas-frontend/src/environments/environments.ts
@@ -6,14 +6,14 @@
     },
   }; */
 
-  /* export const environment = {
+  export const environment = {
     production: false,
     salesforce: {
       baseUrl: 'https://orgfarm-7bd48eb5b5-dev-ed.develop.my.salesforce.com', // Solo la instancia base
-    accessToken: '00DgK000001Krim!AQEAQE5m_HFTdz.Li0LYRavu614sbI4cvcHhf70LR62fzDwYWX.d95ZwXNEoLIqPUXobbJW323H5oqOR1LN58WQ6ENw83ajU', // Reemplaza con tu token de acceso
+      accessToken: '00DgK000001Krim!AQEAQPT9uc7YT4ve1DrtNEmKrvFPXBtEII.svpGu7rXOehm_YGYnsQJtAQS0Yr9D7wSm5nxg0YKKjk231MLccq4cMedorFYh', // Reemplaza con tu token de acceso
       refreshToken: '5Aep8612EC5NxGKVYreGs13NIzFX7k8UaPFqYWXrlgaxCIYFsUid1St_YKp0FebTl7M1DItuphkMnq_XTxrrMIn', // Agrega el Refresh Token
       clientId: '3MVG9rZjd7MXFdLgVUB54wy8BfunvX_6eaKWUMMprEvNxTad6sEc1gUpXtzUAbf_bxaWWutBwd_RVb1VW_TJR', // Reemplaza con tu Consumer Key
       clientSecret: '16A9B63899FA1F1133E8608900F75C1072BAAF411A9CB7DDF968092CD4A6BCE8', // Reemplaza con tu Consumer Secret
       tokenUrl: 'https://login.salesforce.com/services/oauth2/token', // Endpoint para renovar el token
     },
-  }; */
+  }; 
